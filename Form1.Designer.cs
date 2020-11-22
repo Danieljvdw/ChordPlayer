689eb2f67e3bd2488c716de8f77bc34916ddf8a6
Daniel Werf <danielw@bytesnap.co.uk>
2020-11-22 20:02:06 +0000

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ChordPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clmChordChecked = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmChords = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Change timer";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 23);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Chord";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(129, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 46);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "A";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(690, 313);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmChordChecked,
+            this.clmChords});
+            this.dataGridView1.Location = new System.Drawing.Point(244, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 4);
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(419, 295);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellValueChanged);
+            // 
+            // clmChordChecked
+            // 
+            this.clmChordChecked.HeaderText = "Checked";
+            this.clmChordChecked.MinimumWidth = 8;
+            this.clmChordChecked.Name = "clmChordChecked";
+            this.clmChordChecked.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.clmChordChecked.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // clmChords
+            // 
+            this.clmChords.HeaderText = "Chords";
+            this.clmChords.MinimumWidth = 8;
+            this.clmChords.Name = "clmChords";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 163);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Default List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 313);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clmChordChecked;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmChords;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace ChordPlayer
+{
+    public partial class Form1 : Form
+    {
+        private bool propertiesLoaded = false;
+
+        private bool active = false;
+        private int counter = 0;
+        private Random r = new Random();
+
+        private System.Windows.Forms.Timer Timer = null;
+
+        private string[] avaliableChords = { "A", "B", "C", "D", "E", "F", "G", "a", "b", "c", "d", "e", "f", "g" };
+
+        private string currentChord = "";
+        public string CurrentChord
+        {
+            get
+            {
+                return currentChord;
+            }
+            set
+            {
+                currentChord = value;
+                label3.Text = currentChord;
+            }
+        }
+
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (active)
+            {
+                button1.Text = "Start";
+                active = false;
+            }
+            else
+            {
+                button1.Text = "Stop";
+                active = true;
+            }
+        }
+
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (!propertiesLoaded)
+                return;
+
+            if (Properties.Settings.Default.Chords == null)
+            {
+                Properties.Settings.Default.Chords = new System.Collections.Specialized.StringCollection();
+            }
+            if (Properties.Settings.Default.ChordsChecked == null)
+            {
+                Properties.Settings.Default.ChordsChecked = new System.Collections.Specialized.StringCollection();
+            }
+
+            Properties.Settings.Default.Chords.Clear();
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value == null)
+                    dataGridView1.Rows[i].Cells[0].Value = false;
+                if (dataGridView1.Rows[i].Cells[1].Value == null)
+                    dataGridView1.Rows[i].Cells[1].Value = "";
+
+                Properties.Settings.Default.ChordsChecked.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                Properties.Settings.Default.Chords.Add(dataGridView1.Rows[i].Cells[1].Value.ToString());
+            }
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool propertiesPopulated = false;
+
+            if (Properties.Settings.Default.Chords != null)
+            {
+                int i = 0;
+
+                for (i = 0; i < Properties.Settings.Default.Chords.Count; i++)
+                {
+                    bool boxChecked = (Properties.Settings.Default.ChordsChecked[i] == "True");
+                    string chordLabel = Properties.Settings.Default.Chords[i];
+
+                    dataGridView1.Rows.Add(boxChecked, chordLabel);
+                }
+
+                if(i > 0)
+                {
+                    propertiesPopulated = true;
+                }
+            }
+
+            if(!propertiesPopulated)
+            {
+                loadDefaultProperties();
+            }
+
+            propertiesLoaded = true;
+
+            if (Timer == null)
+            {
+                Timer = new Timer();
+                Timer.Interval = 1000;
+                Timer.Tick += Timer_Tick;
+                Timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!active)
+                return;
+
+            counter++;
+
+            if(counter >= numericUpDown1.Value)
+            {
+                pickChord();
+                counter = 0;
+            }
+        }
+
+        private void pickChord()
+        {
+            int numberOfChords = 0;
+            int chordChosen = 0;
+
+            foreach(DataGridViewRow r in dataGridView1.Rows)
+            {
+                if(r.Cells[0].Value != null && (bool)r.Cells[0].Value)
+                {
+                    numberOfChords++;
+                }
+            }
+
+            if (numberOfChords == 0)
+                return;
+
+            chordChosen = r.Next(numberOfChords) + 1;
+
+            foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                if (r.Cells[0].Value != null && (bool)r.Cells[0].Value)
+                {
+                    chordChosen--;
+                }
+
+                if(chordChosen == 0)
+                {
+                    string c = "";
+
+                    try
+                    {
+                        c = (string)r.Cells[1].Value;
+                    }
+                    catch
+                    {
+                        c = Convert.ToString(r.Cells[1].Value);
+                    }
+
+                    CurrentChord = c;
+                    break;
+                }
+            }
+
+            Task.Run(() => synth.Speak(CurrentChord));
+        }
+
+        private void loadDefaultProperties()
+        {
+            dataGridView1.Rows.Clear();
+
+            foreach (string c in avaliableChords)
+            {
+                dataGridView1.Rows.Add(true, c);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            loadDefaultProperties();
+        }
+    }
+}
